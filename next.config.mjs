b4e96213a6291dc9347111e65169bc805a21c68f
nachodeh <nachodeh@gmail.com>
2024-10-05 16:28:58 +0100

    Fix build

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const withSerwist = (await import("@serwist/next")).default({
     swSrc: "app/sw.ts",
     swDest: "public/sw.js",
     cacheOnNavigation: true,
-    additionalPrecacheEntries: [{ url: "/~offline", revision: revision }],
 })
 
 let config = withSerwist(nextConfig);
