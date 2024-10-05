fc696aa4a99d8569585f046d12a095de108e5cc3
nachodeh <nachodeh@gmail.com>
2024-10-05 16:44:25 +0100

    manifest.ts

---
+++ app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from "next"
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: "mentor",
+        short_name: "mentor",
+        icons: [
+            {
+                src: "/logo-maskable.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "maskable",
+            },
+            {
+                src: "/logo.png",
+                sizes: "512x512",
+                type: "image/png",
+            },
+        ],
+        theme_color: "#080E20",
+        background_color: "#9333EA",
+        start_url: "/dashboard",
+        display: "standalone",
+        orientation: "portrait",
+        scope: "/",
+    }
+}
